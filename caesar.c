695e2525bebf49ee7b3da4a75aa3a509420004f3
momenessam9 <momenessam9@users.noreply.github.com>
2019-06-24 14:10:03 +0000

    20190624T140931Z

---
+++ caesar.c
@@ -0,0 +1,42 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+
+int main(int argc, string argv[])
+{
+    
+    if (argc != 2)
+    {
+        printf("Usage: ./caeser key\n");
+        return 1;
+    } 
+    
+    for (int i = 0; i < strlen(argv[1]); i++)
+    {
+        if (isdigit(argv[1][i]))
+        {
+        }
+        else
+        {
+            printf("Usage: ./caeser key\n");
+            return 1;  
+        }
+    }
+    int key = atoi(argv[1]);
+    string text = get_string("plaintaxt: ");
+    for (int i = 0; i < strlen(text); i++)
+    {
+        if (isalpha(text[i]))
+        {
+            int chr = text[i];
+            text[i] = (chr + key);
+            if (text[i] <= 0 || text[i] == 127 || ispunct(text[i]))
+            {
+                text[i] = (chr - key);
+            }
+        }
+    }
+    printf("ciphertext: %s\n", text); 
+}

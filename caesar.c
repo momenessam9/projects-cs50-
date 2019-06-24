f830fefb52c16f981106b718dc7bc5f54ce7fc27
momenessam9 <momenessam9@users.noreply.github.com>
2019-06-24 14:55:32 +0000

    20190624T145504Z

--- caesar.c
+++ caesar.c
@@ -6,13 +6,11 @@
 
 int main(int argc, string argv[])
 {
-    
     if (argc != 2)
     {
         printf("Usage: ./caeser key\n");
         return 1;
     } 
-    
     for (int i = 0; i < strlen(argv[1]); i++)
     {
         if (isdigit(argv[1][i]))
@@ -24,6 +22,7 @@ int main(int argc, string argv[])
             return 1;  
         }
     }
+    
     int key = atoi(argv[1]);
     string text = get_string("plaintext: ");
     for (int i = 0; i < strlen(text); i++)
@@ -31,12 +30,30 @@ int main(int argc, string argv[])
         if (isalpha(text[i]))
         {
             int chr = text[i];
-            text[i] = (chr + key);
+            if (isupper(text[i]))
+            {
+                char cas = tolower(chr);
+                text[i] = cas + key;
+                if (text[i] <= 0 || text[i] == 127 || ispunct(text[i]))
+                {
+                    cas = chr - key;
+                    text[i] = toupper(cas);
+                }
+                else
+                {                  
+                    text[i] = toupper(cas);
+                }
+            }
+            else
+            {
+                text[i] = chr + key;
+            }
             if (text[i] <= 0 || text[i] == 127 || ispunct(text[i]))
             {
-                text[i] = (chr - key);
+                text[i] = chr - key;
             }
         }
     }
     printf("ciphertext: %s\n", text); 
 }
+

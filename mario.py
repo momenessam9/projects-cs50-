79c509cc5cbe74b75a8491df1a9d4c8f72598b03
momenessam9 <momenessam9@users.noreply.github.com>
2019-07-03 16:57:16 +0000

    20190703T165708Z

--- mario.py
+++ mario.py
@@ -1,10 +1,10 @@
-
-height = int(input("height: "))
+from cs50 import get_int
+height = get_int("height: ")
 while True:
     if height > 0 and height <= 8:
         break
     else:
-        height = int(input("height: "))
+        height = get_int("height: ")
         
 i = height - 1
 while (i != -1):
